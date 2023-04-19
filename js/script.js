87d6d625148d80f9cda9c0ce618283714274034d
Sergei Sokolov <sokolov12ssa@gmail.com>
2023-04-19 23:42:44 +0500

    Second commit displaying top 20 movies & shows

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const global = {
 
 // Display Popular Movies
 const displayMovies = async () => {
-    const { results } = await fetchAPIData('/movie/popular')
+    const { results } = await fetchAPIData('movie/popular')
     results.forEach(movie => {
         /* For each movie create a separate div element with all the necessary info from our API 
         and display in the DOM */
@@ -41,6 +41,43 @@ const displayMovies = async () => {
     })
 }
 
+const displayShows = async () => {
+    const { results } = await fetchAPIData('tv/popular')
+    results.forEach(show => {
+        // Create an element and display it in the DOM
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path ? 
+            `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+            `
+            :
+            `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+            `
+            }
+            
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `
+        document.getElementById('popular-shows').appendChild(div)
+    })
+}
+
 // Fetch API Movie Data
 const fetchAPIData = async (endPoint) => {
     const API_KEY = 'e7fc0d311099e5e1dffea6b071e7902b'
@@ -49,7 +86,6 @@ const fetchAPIData = async (endPoint) => {
     const data = res.json()
     return data
 }
-
 
 
 // Change Color Of Active Links
@@ -71,7 +107,7 @@ const init = () => {
             displayMovies()
             break
         case '/shows.html':
-            console.log('Shows')
+            displayShows()
             break
         case '/movie-details.html':
             console.log('Movie details')
